53b70911cebff9a1cae93470d1daa7bef8f8a53f
Iran Eduardo <iranedu@hotmail.com>
2016-07-05 00:18:03 -0300

    Decima Quarta Fase do Projeto (Code Project): Inicio das configurações do menu e tela principais da Aplicação

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,46 +26,457 @@
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
 </head>
-<body>
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
-					<span class="sr-only">Toggle Navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#">Laravel</a>
+
+<body class="page-dashboard">
+<!--[if lt IE 7]>
+  <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+    <div ng-include="'build/views/templates/menu.html'"></div>
+
+    <section class="main">
+	  <div class="container">
+		 <div class="row">
+			<div class="col-xs-9">
+				<header class="header-internal">
+					<div class="row row-xs">
+						<div class="col-xs-8">
+							<h2 class="h4 page-title line-after">
+								<span>Projetos</span>
+							</h2>
+						</div>
+						<div class="col-xs-4">
+							<div class="tools">
+								<div>
+									<label class="select-styled select-styled-default select-styled-sm">
+										<select>
+											<option selected="selected">Todos</option>
+											<option>Nome do projeto</option>
+											<option>Nome do projeto2</option>
+											<option>Nome do projeto3</option>
+											<option>Nome do projeto4</option>
+											<option>Nome do projeto5</option>
+										</select>
+									</label>
+								</div>
+								<div class="btn-group">
+									<a href="" class="btn btn-default btn-sm">
+										<i class="fa fa-th"></i>
+									</a>
+									<a href="" class="btn btn-default btn-sm">
+										<i class="fa fa-align-justify"></i>
+									</a>
+								</div>
+
+								<div class="btn btn-default btn-sm">
+									<i class="fa fa-plus"></i> Projeto
+								</div>
+							</div> <!-- /tools -->
+						</div>
+					</div>
+				</header> <!-- /header-internal -->
+
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="panel panel-default panel-stacked">
+							<div class="panel-heading bg-white no-border-bottom text-boulder">
+								<h3 class="h4 text-boulder">Nome do projeto</h3>
+								<small class="text-upper text-silver">Cliente empresa</small>
+							</div> <!-- /panel-heading bg-white -->
+							<div class="panel-body">
+								<ul class="list-unstyled list-padding text-boulder">
+									<li class="clearfix">
+										Progresso:
+
+										<div class="radial-progress radial-progress-xs radial-progress-warning progresso-inline pull-right" data-progress="50">
+											<div class="circle">
+												<div class="mask full">
+													<div class="fill"></div>
+												</div>
+												<div class="mask half">
+													<div class="fill"></div>
+													<div class="fill fix"></div>
+												</div>
+												<div class="shadow"></div>
+											</div>
+											<div class="inset">
+												<div class="percentage">
+													<div class="numbers">50%</div>
+												</div>
+											</div>
+										</div>
+									</li>
+									<li class="clearfix">
+										Progresso esperado:
+
+										<div class="radial-progress radial-progress-xs radial-progress-warning progresso-inline pull-right" data-progress="100">
+											<div class="circle">
+												<div class="mask full">
+													<div class="fill"></div>
+												</div>
+												<div class="mask half">
+													<div class="fill"></div>
+													<div class="fill fix"></div>
+												</div>
+												<div class="shadow"></div>
+											</div>
+											<div class="inset">
+												<div class="percentage">
+													<div class="numbers">100%</div>
+												</div>
+											</div>
+										</div>
+									</li>
+									<li>
+										Prazo: <span class="pull-right text-orange">Jul 20</span>
+									</li>
+								</ul> <!-- /panel-list-unstyled -->
+							</div> <!-- /panel-body -->
+							<div class="panel-footer panel-footer-xs no-border-top sazerac text-info font-bold">
+								<i class="fa fa-circle"></i> Não iniciou
+								<span class="pull-right">+0</span>
+							</div> <!-- /panel-footer panel-footer-xs sazerac -->
+						</div> <!-- /panel.panel-default -->
+					</div>
+					<div class="col-sm-4">
+						<div class="panel panel-default panel-stacked">
+							<div class="panel-heading bg-white no-border-bottom text-boulder">
+								<h3 class="h4 text-boulder">Nome do projeto2</h3>
+								<small class="text-upper text-silver">Cliente empresa</small>
+							</div> <!-- /panel-heading bg-white -->
+							<div class="panel-body">
+								<ul class="list-unstyled list-padding text-boulder">
+									<li class="clearfix">
+										Progresso:
+
+										<div class="radial-progress radial-progress-xs radial-progress-warning progresso-inline pull-right" data-progress="1">
+											<div class="circle">
+												<div class="mask full">
+													<div class="fill"></div>
+												</div>
+												<div class="mask half">
+													<div class="fill"></div>
+													<div class="fill fix"></div>
+												</div>
+												<div class="shadow"></div>
+											</div>
+											<div class="inset">
+												<div class="percentage">
+													<div class="numbers">1%</div>
+												</div>
+											</div>
+										</div>
+									</li>
+									<li class="clearfix">
+										Progresso esperado:
+
+										<div class="radial-progress radial-progress-xs radial-progress-warning progresso-inline pull-right" data-progress="90">
+											<div class="circle">
+												<div class="mask full">
+													<div class="fill"></div>
+												</div>
+												<div class="mask half">
+													<div class="fill"></div>
+													<div class="fill fix"></div>
+												</div>
+												<div class="shadow"></div>
+											</div>
+											<div class="inset">
+												<div class="percentage">
+													<div class="numbers">90%</div>
+												</div>
+											</div>
+										</div>
+									</li>
+									<li>
+										Prazo: <span class="pull-right text-orange">Jul 20</span>
+									</li>
+								</ul> <!-- /panel-list-unstyled -->
+							</div> <!-- /panel-body -->
+							<div class="panel-footer panel-footer-xs no-border-top sazerac text-success font-bold">
+								<i class="fa fa-circle"></i> Iniciado
+								<span class="pull-right">+100</span>
+							</div> <!-- /panel-footer panel-footer-xs sazerac -->
+						</div> <!-- /panel.panel-default -->
+					</div>
+					<div class="col-sm-4">
+						<div class="panel panel-default panel-stacked">
+							<div class="panel-heading bg-white no-border-bottom text-boulder">
+								<h3 class="h4 text-boulder">Nome do projeto3</h3>
+								<small class="text-upper text-silver">Cliente empresa</small>
+							</div> <!-- /panel-heading bg-white -->
+							<div class="panel-body">
+								<ul class="list-unstyled list-padding text-boulder">
+									<li class="clearfix">
+										Progresso:
+
+										<div class="radial-progress radial-progress-xs radial-progress-warning progresso-inline pull-right" data-progress="35">
+											<div class="circle">
+												<div class="mask full">
+													<div class="fill"></div>
+												</div>
+												<div class="mask half">
+													<div class="fill"></div>
+													<div class="fill fix"></div>
+												</div>
+												<div class="shadow"></div>
+											</div>
+											<div class="inset">
+												<div class="percentage">
+													<div class="numbers">35%</div>
+												</div>
+											</div>
+										</div>
+									</li>
+									<li class="clearfix">
+										Progresso esperado:
+
+										<div class="radial-progress radial-progress-xs radial-progress-warning progresso-inline pull-right" data-progress="90">
+											<div class="circle">
+												<div class="mask full">
+													<div class="fill"></div>
+												</div>
+												<div class="mask half">
+													<div class="fill"></div>
+													<div class="fill fix"></div>
+												</div>
+												<div class="shadow"></div>
+											</div>
+											<div class="inset">
+												<div class="percentage">
+													<div class="numbers">90%</div>
+												</div>
+											</div>
+										</div>
+									</li>
+									<li>
+										Prazo: <span class="pull-right text-orange">Jul 20</span>
+									</li>
+								</ul> <!-- /panel-list-unstyled -->
+							</div> <!-- /panel-body -->
+							<div class="panel-footer panel-footer-xs no-border-top sazerac text-success font-bold">
+								<i class="fa fa-circle"></i> Iniciado
+								<span class="pull-right">+100</span>
+							</div> <!-- /panel-footer panel-footer-xs sazerac -->
+						</div> <!-- /panel.panel-default -->
+					</div>
+					<div class="col-sm-4">
+						<div class="panel panel-default panel-stacked">
+							<div class="panel-heading bg-white no-border-bottom text-boulder">
+								<h3 class="h4 text-boulder">Nome do projeto4</h3>
+								<small class="text-upper text-silver">Cliente empresa</small>
+							</div> <!-- /panel-heading bg-white -->
+							<div class="panel-body">
+								<ul class="list-unstyled list-padding text-boulder">
+									<li class="clearfix">
+										Progresso:
+
+										<div class="radial-progress radial-progress-xs radial-progress-warning progresso-inline pull-right" data-progress="35">
+											<div class="circle">
+												<div class="mask full">
+													<div class="fill"></div>
+												</div>
+												<div class="mask half">
+													<div class="fill"></div>
+													<div class="fill fix"></div>
+												</div>
+												<div class="shadow"></div>
+											</div>
+											<div class="inset">
+												<div class="percentage">
+													<div class="numbers">35%</div>
+												</div>
+											</div>
+										</div>
+									</li>
+									<li class="clearfix">
+										Progresso esperado:
+
+										<div class="radial-progress radial-progress-xs radial-progress-warning progresso-inline pull-right" data-progress="90">
+											<div class="circle">
+												<div class="mask full">
+													<div class="fill"></div>
+												</div>
+												<div class="mask half">
+													<div class="fill"></div>
+													<div class="fill fix"></div>
+												</div>
+												<div class="shadow"></div>
+											</div>
+											<div class="inset">
+												<div class="percentage">
+													<div class="numbers">90%</div>
+												</div>
+											</div>
+										</div>
+									</li>
+									<li>
+										Prazo: <span class="pull-right text-orange">Jul 20</span>
+									</li>
+								</ul> <!-- /panel-list-unstyled -->
+							</div> <!-- /panel-body -->
+							<div class="panel-footer panel-footer-xs no-border-top sazerac text-success font-bold">
+								<i class="fa fa-circle"></i> Iniciado
+								<span class="pull-right">+100</span>
+							</div> <!-- /panel-footer panel-footer-xs sazerac -->
+						</div> <!-- /panel.panel-default -->
+					</div>
+					<div class="col-sm-4">
+						<div class="panel panel-default panel-stacked">
+							<div class="panel-heading bg-white no-border-bottom text-boulder">
+								<h3 class="h4 text-boulder">Nome do projeto5</h3>
+								<small class="text-upper text-silver">Cliente empresa</small>
+							</div> <!-- /panel-heading bg-white -->
+							<div class="panel-body">
+								<ul class="list-unstyled list-padding text-boulder">
+									<li class="clearfix">
+										Progresso:
+
+										<div class="radial-progress radial-progress-xs radial-progress-warning progresso-inline pull-right" data-progress="35">
+											<div class="circle">
+												<div class="mask full">
+													<div class="fill"></div>
+												</div>
+												<div class="mask half">
+													<div class="fill"></div>
+													<div class="fill fix"></div>
+												</div>
+												<div class="shadow"></div>
+											</div>
+											<div class="inset">
+												<div class="percentage">
+													<div class="numbers">35%</div>
+												</div>
+											</div>
+										</div>
+									</li>
+									<li class="clearfix">
+										Progresso esperado:
+
+										<div class="radial-progress radial-progress-xs radial-progress-warning progresso-inline pull-right" data-progress="90">
+											<div class="circle">
+												<div class="mask full">
+													<div class="fill"></div>
+												</div>
+												<div class="mask half">
+													<div class="fill"></div>
+													<div class="fill fix"></div>
+												</div>
+												<div class="shadow"></div>
+											</div>
+											<div class="inset">
+												<div class="percentage">
+													<div class="numbers">90%</div>
+												</div>
+											</div>
+										</div>
+									</li>
+									<li>
+										Prazo: <span class="pull-right text-orange">Jul 20</span>
+									</li>
+								</ul> <!-- /panel-list-unstyled -->
+							</div> <!-- /panel-body -->
+							<div class="panel-footer panel-footer-xs no-border-top sazerac text-danger font-bold">
+								<i class="fa fa-circle"></i> Atrasado
+								<span class="pull-right">-30</span>
+							</div> <!-- /panel-footer panel-footer-xs sazerac -->
+						</div> <!-- /panel.panel-default -->
+					</div>
+				</div>
 			</div>
+			<div class="col-xs-3">
+				<h5>Últimas atualizações</h5>
 
-			<div class="collapse navbar-collapse" id="navbar">
-				<ul class="nav navbar-nav">
-					<li><a href="{{ url('/') }}">Welcome</a></li>
-				</ul>
-
-				<ul class="nav navbar-nav navbar-right">
-					@if(auth()->guest())
-						@if(!Request::is('auth/login'))
-							<li><a href="{{ url('/auth/login') }}">Login</a></li>
-						@endif
-						@if(!Request::is('auth/register'))
-							<li><a href="{{ url('/auth/register') }}">Register</a></li>
-						@endif
-					@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ auth()->user()->name }} <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
+				<br/>
+				<div class="mini-timeline">
+					<div class="list-item list-item-stacked text-boulder">
+						<div class="clearfix">
+							<img src="http://mmn.silasribas.com.br/bundles/sonbase/geral/images/anonymous.png?2" alt="" class="avatar-sm rounded"/>
+							<p>
+								<strong>Mudança de status em um projeto</strong>
+								<br/>
+								<small>há alguns segundos</small>
+								<br/>
+								<span class="text-info">O status no novo site foi alterado para vencido</span>
+							</p>
+						</div>
+					</div> <!-- /list-item -->
+					<div class="list-item list-item-stacked text-boulder">
+						<div class="clearfix">
+							<img src="http://mmn.silasribas.com.br/bundles/sonbase/geral/images/anonymous.png?2" alt="" class="avatar-sm rounded"/>
+							<p>
+								<strong>Mudança de status em um projeto</strong>
+								<br/>
+								<small>há alguns segundos</small>
+								<br/>
+								<span class="text-info">O status no novo site foi alterado para vencido</span>
+							</p>
+						</div>
+					</div> <!-- /list-item -->
+					<div class="list-item list-item-stacked text-boulder">
+						<div class="clearfix">
+							<img src="http://mmn.silasribas.com.br/bundles/sonbase/geral/images/anonymous.png?2" alt="" class="avatar-sm rounded"/>
+							<p>
+								<strong>Mudança de status em um projeto</strong>
+								<br/>
+								<small>há alguns segundos</small>
+								<br/>
+								<span class="text-info">O status no novo site foi alterado para vencido</span>
+							</p>
+						</div>
+					</div> <!-- /list-item -->
+					<div class="list-item list-item-stacked text-boulder">
+						<div class="clearfix">
+							<img src="http://mmn.silasribas.com.br/bundles/sonbase/geral/images/anonymous.png?2" alt="" class="avatar-sm rounded"/>
+							<p>
+								<strong>Mudança de status em um projeto</strong>
+								<br/>
+								<small>há alguns segundos</small>
+								<br/>
+								<span class="text-info">O status no novo site foi alterado para vencido</span>
+							</p>
+						</div>
+					</div> <!-- /list-item -->
+					<div class="list-item list-item-stacked text-boulder">
+						<div class="clearfix">
+							<img src="http://mmn.silasribas.com.br/bundles/sonbase/geral/images/anonymous.png?2" alt="" class="avatar-sm rounded"/>
+							<p>
+								<strong>Mudança de status em um projeto</strong>
+								<br/>
+								<small>há alguns segundos</small>
+								<br/>
+								<span class="text-info">O status no novo site foi alterado para vencido</span>
+							</p>
+						</div>
+					</div> <!-- /list-item -->
+					<div class="list-item list-item-stacked text-boulder">
+						<div class="clearfix">
+							<img src="http://mmn.silasribas.com.br/bundles/sonbase/geral/images/anonymous.png?2" alt="" class="avatar-sm rounded"/>
+							<p>
+								<strong>Mudança de status em um projeto</strong>
+								<br/>
+								<small>há alguns segundos</small>
+								<br/>
+								<span class="text-info">O status no novo site foi alterado para vencido</span>
+							</p>
+						</div>
+					</div> <!-- /list-item -->
+				</div>
 			</div>
 		</div>
-	</nav>
 
-	<div ng-view></div>
+		 <div ng-view></div>
+
+	  </div>
+    </section>
+    <footer class="footer-global">
+	  <div class="container">
+		 <div class="row">
+			<div class="col-xs-12">
+				<div class="text-center">&copy; Project Manager - 2015</div>
+			</div>
+		  </div>
+	  </div>
+    </footer>
 
 	@if(Config::get('app.debug'))
 		<script src="{{ asset("build/js/vendor/jquery.min.js") }}"></script>
